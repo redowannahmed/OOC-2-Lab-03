406265d137a9f0d8983d63c9468f65e918c13a35
Redowan Ahmed <161846618+redowannahmed@users.noreply.github.com>
2024-09-25 07:59:19 -0700

    Update NumberPrinterTest.java

--- Task1/NumberPrinterTest.java
+++ Task1/NumberPrinterTest.java
@@ -19,14 +19,37 @@ public class NumberPrinterTest {
     }
 
     @Test
-    public void testPrimeEleven ()
-    {
-        assertEquals("EvenPrimeEleven", printer.printWord(22, false));
+    public void testEvenUppercase() {
+        assertEquals("EVEN", printer.printWord(8, true));
     }
 
     @Test
-    public void testPrimeELEVEN ()
-    {
-        assertEquals("EVENPRIMEELEVEN", printer.printWord(22, true));
+    public void testPrimeEleven() {
+        assertEquals("PrimeEleven", printer.printWord(22, false));
+    }
+
+    @Test
+    public void testPrimeElevenUppercase() {
+        assertEquals("PRIMEELEVEN", printer.printWord(22, true));
+    }
+
+    @Test
+    public void testLuckyThirteen() {
+        assertEquals("LuckyThirteen", printer.printWord(13, false));
+    }
+
+    @Test
+    public void testEvenPrimeLucky() {
+        assertEquals("EvenPrimeLucky", printer.printWord(286, false)); 
+    }
+
+    @Test
+    public void testOddOneOut() {
+        assertEquals("OddOneOut", printer.printWord(7, false));
+    }
+
+    @Test
+    public void testOddOneOutUppercase() {
+        assertEquals("ODDONEOUT", printer.printWord(7, true));
     }
 }
