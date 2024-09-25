600af13d286eea785f43267bb9be76dcfc7850c1
Redowan Ahmed <161846618+redowannahmed@users.noreply.github.com>
2024-09-25 07:56:40 -0700

    Update NumWordPrinter.java

--- Task1/NumWordPrinter.java
+++ Task1/NumWordPrinter.java
@@ -4,13 +4,23 @@ public class NumWordPrinter {
     public String printWord (int n, boolean toUpperCase)
     {
         String ans;
-        if (n % 2 == 0)
-        {
+        if (n % 2 == 0 && n % 11 == 0 && n % 13 == 0) {
+            ans = "EvenPrimeLucky";
+        }
+        else if (n % 2 == 0 && n % 11 == 0) {
+            ans = "EvenPrimeEleven";
+        }
+        else if (n % 2 == 0) {
             ans = "Even";
         }
-        else 
-        {
-            ans = null;
+        else if (n % 11 == 0) {
+            ans = "PrimeEleven";
+        }
+        else if (n % 13 == 0) {
+            ans = "LuckyThirteen";
+        }
+        else {
+            ans = "OddOneOut";
         }
 
         if (toUpperCase)
