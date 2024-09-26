e6407212c41e34f4456b25cc2236a7d49daa24be
Redowan Ahmed <161846618+redowannahmed@users.noreply.github.com>
2024-09-26 00:01:26 -0700

    Update Testing.java

--- Task2/Testing.java
+++ Task2/Testing.java
@@ -15,4 +15,27 @@ public class Testing {
         st.push(6);
         assertEquals(6, st.max());
     }
+
+    @Test
+    public void test2 ()
+    {
+        st.push(3);
+        st.push (2);
+        st.push (5);
+        st.push(6);
+        st.pop ();
+
+        assertEquals(5, st.max());
+    }
+
+    @Test
+    public void test3 ()
+    {
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(2);
+
+        assertEquals(3, st.max());
+    }
 }
